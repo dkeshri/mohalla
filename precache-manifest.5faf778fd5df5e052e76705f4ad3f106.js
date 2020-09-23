8b9eb7f28d4c8e962167c45babbe1eac6e25586a
Deepak Keshri <dkeshridev@gmail.com>
2020-09-23 10:43:25 +0530

    Updates

---
+++ precache-manifest.5faf778fd5df5e052e76705f4ad3f106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52a8badd6331c7ee323c3b66fb38636",
+    "url": "/mohalla/index.html"
+  },
+  {
+    "revision": "9c5882c733caa534d1d1",
+    "url": "/mohalla/static/css/main.c74ed4af.chunk.css"
+  },
+  {
+    "revision": "5a4b56d458477af6e3fc",
+    "url": "/mohalla/static/js/2.73d0341a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mohalla/static/js/2.73d0341a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5882c733caa534d1d1",
+    "url": "/mohalla/static/js/main.53888c86.chunk.js"
+  },
+  {
+    "revision": "80b5099b03246ddc153a",
+    "url": "/mohalla/static/js/runtime-main.6a2fae20.js"
+  },
+  {
+    "revision": "9424f2f835646842678cd4b8e34cd23e",
+    "url": "/mohalla/static/media/bg.9424f2f8.jpg"
+  }
+]);
