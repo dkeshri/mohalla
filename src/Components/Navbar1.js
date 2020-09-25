c1402ed0ef3daab27182e77a129a79b1351c0a7d
Deepak Keshri <dkeshridev@gmail.com>
2020-09-25 16:00:20 +0530

    nav bar modify

--- src/Components/Navbar1.js
+++ src/Components/Navbar1.js
@@ -10,47 +10,91 @@ function Navbar1() {
                 <div className="nav-links">
                     <ul>
                         <li className="nav-link"><a href="#">Home <i className="fa fa-caret-down" aria-hidden="true"></i></a>
-                        <div className="dropdown">
-                                    <ul>
-                                        <li className="dropdown-link"><a href="#">Link1</a></li>
-                                        <li className="dropdown-link"><a href="#">Link2</a></li>
-                                        <li className="dropdown-link"><a href="#">Link3<i className="fa fa-caret-down" aria-hidden="true"></i></a>
-                                            <div className="dropdown second">
-                                                <ul>
-                                                    <li className="dropdown-link"><a href="#">Link1</a></li>
-                                                    <li className="dropdown-link"><a href="#">Link2</a></li>
-                                                    <li className="dropdown-link"><a href="#">Link3</a></li>
-                                                    <li className="dropdown-link"><a href="#">Link4</a></li>
-                                                    <li className="dropdown-link"><a href="#">More<i className="fa fa-caret-down" aria-hidden="true"></i></a>
-                                                        <div className="dropdown second">
-                                                            <ul>
-                                                                <li className="dropdown-link"><a href="#">Link1</a></li>
-                                                                <li className="dropdown-link"><a href="#">Link2</a></li>
-                                                                <li className="dropdown-link"><a href="#">Link3</a></li>
-                                                                <li className="dropdown-link"><a href="#">Link4</a></li>
-                                                                <div className="arrow"></div>
-                                                            </ul>
-                                                        </div>
-                                                    </li>
-                                                    <div className="arrow"></div>
-                                                </ul>
-                                            </div>
-                                        </li>
-                                        <li className="dropdown-link"><a href="#">Link4</a></li>
-                                        <li className="dropdown-link"><a href="#">Link5</a></li>
-                                        <div className="arrow"></div>
-                                    </ul>
-                                </div>
+                            <div className="dropdown">
+                                <ul>
+                                    <li className="dropdown-link"><a href="#">Link1</a></li>
+                                    <li className="dropdown-link"><a href="#">Link2</a></li>
+                                    <li className="dropdown-link"><a href="#">Link3<i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                                        <div className="dropdown second">
+                                            <ul>
+                                                <li className="dropdown-link"><a href="#">Link1</a></li>
+                                                <li className="dropdown-link"><a href="#">Link2</a></li>
+                                                <li className="dropdown-link"><a href="#">Link3</a></li>
+                                                <li className="dropdown-link"><a href="#">Link4</a></li>
+                                                <li className="dropdown-link"><a href="#">More<i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                                                    <div className="dropdown second">
+                                                        <ul>
+                                                            <li className="dropdown-link"><a href="#">Link1</a></li>
+                                                            <li className="dropdown-link"><a href="#">Link2</a></li>
+                                                            <li className="dropdown-link"><a href="#">Link3</a></li>
+                                                            <li className="dropdown-link"><a href="#">Link4</a></li>
+                                                            <div className="arrow"></div>
+                                                        </ul>
+                                                    </div>
+                                                </li>
+                                                <div className="arrow"></div>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li className="dropdown-link"><a href="#">Link4</a></li>
+                                    <li className="dropdown-link"><a href="#">Link5</a></li>
+                                    <div className="arrow"></div>
+                                </ul>
+                            </div>
                         </li>
-                        <li className="nav-link"><a href="#">Portfolio <i className="fa fa-caret-down" aria-hidden="true"></i></a></li>
-                        <li className="nav-link"><a href="#">Services <i className="fa fa-caret-down" aria-hidden="true"></i></a></li>
-                        <li className="nav-link"><a href="#">Features <i className="fa fa-caret-down" aria-hidden="true"></i></a></li>
+                        <li className="nav-link"><a href="#">Portfolio <i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                            <div className="dropdown">
+                                <ul>
+                                    <li className="dropdown-link"><a href="#">Link1</a></li>
+                                    <li className="dropdown-link"><a href="#">Link2</a></li>
+                                    <li className="dropdown-link"><a href="#">Link3<i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                                        <div className="dropdown second">
+                                            <ul>
+                                                <li className="dropdown-link"><a href="#">Link1</a></li>
+                                                <li className="dropdown-link"><a href="#">Link2</a></li>
+                                                <li className="dropdown-link"><a href="#">Link3</a></li>
+                                                <li className="dropdown-link"><a href="#">Link4</a></li>
+                                                <div className="arrow"></div>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li className="dropdown-link"><a href="#">Link4</a></li>
+                                    <div className="arrow"></div>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="nav-link"><a href="#">Services <i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                            <div className="dropdown">
+                                <ul>
+                                    <li className="dropdown-link"><a href="#">Link1</a></li>
+                                    <li className="dropdown-link"><a href="#">Link2</a></li>
+                                    <li className="dropdown-link"><a href="#">Link3</a></li>
+                                    <li className="dropdown-link"><a href="#">Link4</a></li>
+                                    <div className="arrow"></div>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="nav-link"><a href="#">Features <i className="fa fa-caret-down" aria-hidden="true"></i></a>
+                            <div className="dropdown">
+                                <ul>
+                                    <li className="dropdown-link"><a href="#">Link1</a></li>
+                                    <li className="dropdown-link"><a href="#">Link2</a></li>
+                                    <li className="dropdown-link"><a href="#">Link3</a></li>
+                                    <li className="dropdown-link"><a href="#">Link4</a></li>
+                                    <div className="arrow"></div>
+                                </ul>
+                            </div>
+                        </li>
                     </ul>
                 </div>
                 <div className="extra-btn">
-                    <a href="#" className="btn transparent">Login</a>
                     <a href="#" className="btn solid">Logout</a>
                 </div>
+            </div>
+            <div className="hamburger">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
             </div>
         </nav >
     )
---
+++ src/Components/myinfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../assets/css/myinfo.css'
+function myinfo() {
+    return (
+        <div >
+            <span>I am a Software developer.</span>
+        </div>
+    )
+}
+
+export default myinfo
